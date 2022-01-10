1238bad105da6e031572a5d97e62977761434a9a
ram <ramthemarimuthu@gmail.com>
2022-01-10 17:49:32 -0500

    add missing imports to signup and login

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function SignUp() {
   const [user_id, setUserId] = userState('');
