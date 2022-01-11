f33492b5504589df96f4203d5a7a05b0136252ca
ram <ramthemarimuthu@gmail.com>
2022-01-10 19:58:35 -0500

    companies component started. need to render the companies

--- client/components/Companies.jsx
+++ client/components/Companies.jsx
@@ -1,4 +1,23 @@
-import React from 'react';
-function Companies() {}
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useLocation } from 'react-router-dom';
+
+function Companies(props) {
+  const { state } = useLocation();
+  const [orgs, setOrgs] = useState('');
+
+  useEffect(() => {
+    //[{"organization":"facebook"},{"organization":"testing"},{"organization":"codesmith"}]
+    try {
+      const response = await axios.get('/api/questions/org');
+      //array of orgs eg ['facebook', 'testing', 'codesmith']
+      setOrgs(response.map((obj) => obj[organization]));
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  return <div></div>;
+}
 
 export default Companies;
