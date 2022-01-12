fa9df0c11ceda77f44b2d727b290a5d436283bbd
ram <ramthemarimuthu@gmail.com>
2022-01-11 19:56:43 -0500

    question modal working

--- client/components/Question.jsx
+++ client/components/Question.jsx
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import reactDom from 'react-dom';
+import Modal from './Modal.jsx';
 function Question(props) {
   const { organization, qdetails, qtitle, qtype } = props.props;
+  const [showModal, setShowModal] = useState(false);
+
   const styles = {
     display: 'flex',
     flexDirection: 'row',
+    backgroundColor: 'red',
+  };
+
+  const toggleModal = () => {
+    setShowModal(true);
   };
   return (
-    <div style={styles}>
-      <p>{organization}</p>
-      <p>{qtitle}</p>
-      <p>{qdetails}</p>
-      <p>{qtype}</p>
+    <div>
+      <div
+        style={styles}
+        onClick={() => {
+          toggleModal();
+          console.log('adf');
+        }}
+      >
+        <p>{organization}</p>
+        <p>{qtitle}</p>
+        <p>{qdetails}</p>
+        <p>{qtype}</p>
+      </div>
+      {showModal ? (
+        <Modal
+          props={props.props}
+          showModal={showModal}
+          setShowModal={setShowModal}
+        />
+      ) : null}
     </div>
   );
 }
